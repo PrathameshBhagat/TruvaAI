4227ad52fcf4dea7b4e30be60b4f9113ffca85ea
Prathamesh Bhagat <prathameshbhagat0608@gmail.com>
2025-03-29 15:20:35 +0530

    ddd

---
+++ app/src/main/java/com/example/truvaai/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.truvaai;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://www.truva.ai/");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+}
