5e50d710983386318eacae5b20fcf984ddae788c
Prathamesh Bhagat <prathameshbhagat0608@gmail.com>
2025-03-29 16:00:38 +0530

    aa

--- app/src/main/java/com/example/truvaai/MainActivity.java
+++ app/src/main/java/com/example/truvaai/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.truvaai;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -11,16 +15,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.Button;
 
+public class MainActivity extends AppCompatActivity {
+    WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView = (WebView) findViewById(R.id.webview);
+        Button b  = findViewById(R.id.button);
+        b.setOnClickListener(v -> {
+            Intent i = new Intent(Intent.ACTION_DIAL);
+            i.setData(Uri.parse("tel:+917058031648"));
+            startActivity(i);
+        });
+
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.loadUrl("http://www.truva.ai/");
         myWebView.setWebViewClient(new WebViewClient());
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check whether the key event is the Back button and if there's history.
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        // If it isn't the Back button or there's no web page history, bubble up to
+        // the default system behavior. Probably exit the activity.
+        return super.onKeyDown(keyCode, event);
     }
 }
